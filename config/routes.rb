6717f213742b88418b6f595a93bf40c5a1589778
Gabriel Teles <gab.teles@hotmail.com>
2018-06-16 11:36:43 -0300

    feat(auth) authentication basics

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, path: 'auth', path_names: { 
+    sign_in: 'login', 
+    sign_out: 'logout', 
+    password: 'secret', 
+    confirmation: 'verification', 
+    unlock: 'unblock', 
+    registration: 'register', 
+    sign_up: 'signup' 
+  }
+  
   root 'landing#index'
 end
